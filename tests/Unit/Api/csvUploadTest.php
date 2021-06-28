8bb7356c8a7a12a743c03d20b7a812c03fa70d0a
Nestor <correo.solutionpc@gmail.com>
2021-06-28 06:44:09 +0100

    Refactor tests. Also test files are deleted after tests

--- tests/Unit/Api/csvUploadTest.php
+++ tests/Unit/Api/csvUploadTest.php
@@ -1,53 +1,55 @@
 <?php
-
 namespace Tests\Unit\Api;
 
 use Tests\TestCase;
 use Illuminate\Http\UploadedFile;
-use App\Models\UserCsv;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Storage;
+
+use App\Models\UserCsv;
 
 class csvUploadTest extends TestCase {
     use RefreshDatabase;
 
 
     public function testNoFileAttachedReturns400(){
-        $apiResponse = $this->post('api/csvUpload', ['file' => '']);
-        $apiResponse->assertStatus(400);
+        $this->assertStatusOnAndDeleteTestFile(400);
     }
 
 
     public function testWrongExtensionReturns415(){
         $file = UploadedFile::fake()->create('document.txt', 50, 'text/plain');
-        $apiResponse = $this->post('api/csvUpload', ['file' => $file]);
-
-        $apiResponse->assertStatus(415);
+        $this->assertStatusOnAndDeleteTestFile(415, $file);
     }
 
 
     public function testCorrectCSVReturns200(){
         $file = UploadedFile::fake()->create('document.csv', 50, 'text/csv');
-        $apiResponse = $this->post('api/csvUpload', ['file' => $file]);
-
-        $apiResponse->assertStatus(200);
+        $this->assertStatusOnAndDeleteTestFile(200, $file);
     }
 
 
-    public function testCSVGetsStoredOnFilesystem(){
-        $fileNameToTest = 'document.csv';
+    private function assertStatusOnAndDeleteTestFile($statusCodeToAssert, $file = '')
+    {
+        $apiResponse = $this->post('api/csvUpload', ['file' => $file]);
+        $apiResponse->assertStatus($statusCodeToAssert);
+        if (!empty($file))
+            Storage::disk('testing')->delete($file->hashName());
+    }
 
+
+    public function testCSVGetsStoredOnFilesystem($fileNameToTest = ''){
+        $fileNameToTest = !empty($fileNameToTest) ? $fileNameToTest : 'document.csv';
         $file = UploadedFile::fake()->create($fileNameToTest, 50, 'text/csv');
         $this->post('api/csvUpload', ['file' => $file]);
         Storage::disk('testing')->assertExists($file->hashName());
+        Storage::disk('testing')->delete($file->hashName());
     }
 
 
     public function testCSVGetsStoredOnDatabase(){
         $fileNameToTest = 'document.csv';
-
-        $file = UploadedFile::fake()->create($fileNameToTest, 50, 'text/csv');
-        $this->post('api/csvUpload', ['file' => $file]);
+        $this->testCSVGetsStoredOnFilesystem($fileNameToTest);
 
         $mockedUserId = 1; //Todo: Test with authenticated UserId
         $uploadedCSV = UserCsv::where('filename', 'like', "%$fileNameToTest")
